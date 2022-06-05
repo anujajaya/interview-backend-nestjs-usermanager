b990638edd65c2d7989fbbf714b52edb374fe9e8
anujajaya <iamanujajaya@gmail.com>
2022-06-05 21:33:35 +0530

    added usermodule

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { User } from './typeorm/User';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { UsersService } from './users/users.service';
+
+@Module({
+  imports: [
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'anuja',
+      password: '1234567890',
+      database: 'users',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
